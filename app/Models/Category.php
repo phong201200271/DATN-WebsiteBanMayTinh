8e7ab2ebc303f516b425aeff3b92b4eab12668d0
Phong <hongphongle774@gmail.com>
2024-03-22 09:24:59 +0700

    add category repository

---
+++ app/Models/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $table = 'category';
+
+    protected $fillable = [
+        'name',
+        'category_parent_id'
+    ];
+
+    public function product() {
+        return $this->hasMany(Product::class,'category_id','category_id');
+    }
+}
