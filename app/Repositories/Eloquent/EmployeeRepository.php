b8304b01529445306c759fc2aa12f050ceaae822
Phong <hongphongle774@gmail.com>
2024-03-22 09:27:53 +0700

    add employee repository

---
+++ app/Repositories/Eloquent/EmployeeRepository.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories\Eloquent;
+use App\Repositories\EmployeeRepositoryInterface;
+
+class EmployeeRepository extends BaseRepository implements EmployeeRepositoryInterface {
+
+}
