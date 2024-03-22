42c999acad063755382100e13f6c571e062a1064
Phong <hongphongle774@gmail.com>
2024-03-22 09:24:41 +0700

    add brand model

---
+++ app/Models/Brand.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Brand extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $table = 'brand';
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function product() {
+        return $this->hasMany(Product::class,'brand_id','brand_id');
+    }
+}
