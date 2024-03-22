3a29786181bd295d4bf85ffadf48cab7b8b05c2c
Phong <hongphongle774@gmail.com>
2024-03-22 09:26:36 +0700

    add detail input invoice repository

---
+++ app/Repositories/Eloquent/DetailInputInvoiceRepository.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories\Eloquent;
+use App\Repositories\DetailInputInvoiceRepositoryInterface;
+
+class DetailInputInvoiceRepository extends BaseRepository implements DetailInputInvoiceRepositoryInterface {
+
+}
