5d7126b1f89009a1b093743f83021166e5e5156e
Phong <hongphongle774@gmail.com>
2024-03-22 09:28:29 +0700

    add input invoice repository

---
+++ app/Repositories/Eloquent/InputInvoiceRepository.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories\Eloquent;
+use App\Repositories\InputInvoiceRepositoryInterface;
+
+class InputInvoiceRepository extends BaseRepository implements InputInvoiceRepositoryInterface {
+
+}
