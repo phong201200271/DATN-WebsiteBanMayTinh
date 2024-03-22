45529f2740e10bb1693b816df662f412e32abc96
Phong <hongphongle774@gmail.com>
2024-03-22 09:27:22 +0700

    add detail purchase invoice repository

---
+++ app/Repositories/Eloquent/DetailPurchaseInvoiceRepository.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories\Eloquent;
+use App\Repositories\DetailPurchaseInvoiceRepositoryInterface;
+
+class DetailPurchaseInvoiceRepository extends BaseRepository implements DetailPurchaseInvoiceRepositoryInterface {
+
+}
