ad320c50ddb7e864318a523d915d0e65b1c0410b
Phong <hongphongle774@gmail.com>
2024-03-22 09:22:20 +0700

    add route login and index for admin and user

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',[UserController::class,'getViewHomePage'])->name('home')->middleware('auth');
+
+Route::get('/403',[UserController::class,'getView403'])->name('403');
+
+Route::prefix('admin')->group(function () {
+
+   Route::get('/login',[AdminController::class,'getViewLoginAdmin'])->name('viewAdminLogin')->middleware('check.login.admin');
+
+   Route::post('/login')->name('adminLogin')->middleware('auth.admin');
+
+   Route::post('/logout',[AdminController::class,'logout'])->name('adminLogout');
+
+   Route::middleware('check.is.admin')->group(function () {
+       Route::get('/dashboard',[AdminController::class,'getViewDashboard'])->name('dashboard');
+   });
 });
+
+Route::get('/login',[UserController::class,'getViewLoginUser'])->name('viewLogin');
+
+Route::post('/login')->name('login')->middleware('auth.user');
+
