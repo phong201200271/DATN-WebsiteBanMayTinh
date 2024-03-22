b4c4393885f2059da804efb317407d357d5e464a
Phong <hongphongle774@gmail.com>
2024-03-22 09:30:02 +0700

    add product price repository

---
+++ app/Repositories/Eloquent/ProductPriceRepository.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories\Eloquent;
+use App\Repositories\ProductPriceRepositoryInterface;
+
+class ProductPriceRepository extends BaseRepository implements ProductPriceRepositoryInterface {
+
+}
