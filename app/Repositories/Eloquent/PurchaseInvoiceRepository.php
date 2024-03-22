85b95686abca0de52ccf86d2b0358122232ceca4
Phong <hongphongle774@gmail.com>
2024-03-22 09:30:59 +0700

    add purchase invoice repository

---
+++ app/Repositories/Eloquent/PurchaseInvoiceRepository.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories\Eloquent;
+use App\Repositories\PurchaseInvoiceRepositoryInterface;
+
+class PurchaseInvoiceRepository extends BaseRepository implements PurchaseInvoiceRepositoryInterface {
+
+}
