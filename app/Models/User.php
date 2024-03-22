2e1fe769baceca624ea72073d26b948e82ef7e1a
Phong <hongphongle774@gmail.com>
2024-03-22 09:21:58 +0700

    fix model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,18 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable,SoftDeletes;
+
+    protected $table = 'user';
+
+    protected $guarded = 'web';
 
     /**
      * The attributes that are mass assignable.
@@ -21,24 +26,17 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone_number',
+        'address',
+        'status',
+        'remember_token'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public function comment() {
+        return $this->hasMany(Comment::class,'user_id','user_id');
+    }
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function purchaseInvoice() {
+        return $this->hasMany(PurchaseInvoice::class,'user_id','user_id');
+    }
 }
