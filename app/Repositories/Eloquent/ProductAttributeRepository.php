3d4a24ee51a5fa322be08dc9cfcca6e83a6db331
Phong <hongphongle774@gmail.com>
2024-03-22 09:29:28 +0700

    add product attribute repository

---
+++ app/Repositories/Eloquent/ProductAttributeRepository.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories\Eloquent;
+use App\Repositories\ProductAttributeRepositoryInterface;
+
+class ProductAttributeRepository extends BaseRepository implements ProductAttributeRepositoryInterface {
+
+}
