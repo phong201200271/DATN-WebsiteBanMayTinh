5e08a5586a63d64edd2729f7376ed5972077de39
Phong <hongphongle774@gmail.com>
2024-03-22 09:25:12 +0700

    add category repository

---
+++ app/Repositories/Eloquent/CategoryRepository.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories\Eloquent;
+use App\Repositories\CategoryRepositoryInterface;
+
+class CategoryRepository extends BaseRepository implements CategoryRepositoryInterface {
+
+}
