1c7b24034fd0c6457347682743c30b130d4f8937
Phong <hongphongle774@gmail.com>
2024-03-22 09:26:54 +0700

    add detail product repository

---
+++ app/Repositories/Eloquent/DetailProductRepository.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories\Eloquent;
+use App\Repositories\DetailProductRepositoryInterface;
+
+class DetailProductRepository extends BaseRepository implements DetailProductRepositoryInterface {
+
+}
