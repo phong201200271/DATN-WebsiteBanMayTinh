6e2d1c3489fd679dd9393ee54361d184921377fe
Phong <hongphongle774@gmail.com>
2024-03-22 09:24:54 +0700

    add brand repository

---
+++ app/Repositories/Eloquent/BrandRepository.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Repositories\Eloquent;
+use App\Repositories\BrandRepositoryInterface;
+
+class BrandRepository extends BaseRepository implements BrandRepositoryInterface {
+
+}
